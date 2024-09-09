942885aeac1ca0f93eafd51f48bafbac4509f705
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-09 19:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //your JS code here. If required.
-let str = window.location.href;
-let n = str.length;
+//let str = window.location.href;
+//let n = str.length;
 
-alert("The length of the URL is:" + n);
+//alert("The length of the URL is:" + n);
+cy.url().then((url) => {
+  let n = url.length;
+  cy.log("The length of the URL is: " + n);
+});
