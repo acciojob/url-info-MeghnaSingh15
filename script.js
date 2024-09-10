f8355abfe2df0aff79a45fa609a190d6c97e8993
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-10 09:58:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //your JS code here. If required.
+() => { 
+    cy.visit(baseUrl); 
+    cy.on("window:alert", str => { 
+        const expectedAlerts = ["The length of the URL is: 12"]; // update this line
+        expect(expectedAlerts).to.include(str); 
+    }); 
+}
 let str = location.href;
 let n = str.length;
 
